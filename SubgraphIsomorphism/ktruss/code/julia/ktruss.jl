7cdee99b7516988d406c0b02fe8f309bd9f4cd72
Tony Kelman <tony@kelman.net>
2017-04-11 23:21:12 -0400

    staying as a cholmod sparse the whole time isnt working

--- SubgraphIsomorphism/ktruss/code/julia/ktruss.jl
+++ SubgraphIsomorphism/ktruss/code/julia/ktruss.jl
@@ -46,38 +46,26 @@ function ktruss(inc_mtx_file, k)
     t_read_inc=@elapsed ii = readdlm( inc_mtx_file, '\t', Int64)
     println("incidence matrix read time : ", t_read_inc)
 
-    t_create_inc=@elapsed E = sparse( ii[:,1], ii[:,2], ii[:,3] )
+    t_create_inc=@elapsed E = Chol.Sparse(sparse( ii[:,1], ii[:,2], ii[:,3] ))
     println("sparse adj. matrix creation time : ", t_create_inc)
 
-    # hoist field access (shouldn't be necessary on julia >= 0.5)
-    #E_colptr = E.colptr
-    #E_rowval = E.rowval
-    #E_nzval  = E.nzval
-
-    #
     tic()
     m,n = size(E)
     x, xc = calcx(E, m, n, k)
-    while sum(xc) != sum( any(E,2) )
+    E_ptr = unsafe_load(E.p)
+    while sum(xc) != countnz(E * ones(n))
         # set elements of E in rows where x is true to 0, E[find(x), :] = 0
-        xcrows = find(xc)
-        Ekeep = E[xcrows, :]
-        E = SparseMatrixCSC(m, n, Ekeep.colptr, xcrows[Ekeep.rowval], Ekeep.nzval)
+        #xcrows = find(xc)
+        #Ekeep = E[xcrows, :]
+        #E = SparseMatrixCSC(m, n, Ekeep.colptr, xcrows[Ekeep.rowval], Ekeep.nzval)
         #num_deleted = 0
-        #@inbounds for col in 1:size(E, 2)
-        #    for k in E_colptr[col] + num_deleted : E_colptr[col+1]-1
-        #        if x[E_rowval[k]]
-        #            num_deleted += 1
-        #        elseif num_deleted > 0
-        #            E_rowval[k - num_deleted] = E_rowval[k]
-        #            E_nzval[k - num_deleted] = E_nzval[k]
-        #        end
-        #    end
-        #    E_colptr[col+1] -= num_deleted
-        #end
-        #resize!(E_rowval, length(E_rowval) - num_deleted)
-        #resize!(E_nzval, length(E_nzval) - num_deleted)
-        #E[find(x), :] = 0
+        @inbounds for col in 1:n
+            for j in colptr0(E, col) + 1 : colptr0(E, col+1)
+                if x[rowval(E, j)]
+                    unsafe_store!(E_ptr.x, 0, j)
+                end
+            end
+        end
         x, xc = calcx(E, m, n, k)
     end
     
